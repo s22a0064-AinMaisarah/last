656432217782d2235c4d4a25bffe47afd00fa16a
s22a0064-AinMaisarah <s22a0064@siswa.umk.edu.my>
2026-01-04 03:55:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="School Traffic Analysis",
-    page_icon="🚦",
+    page_title="Traffic Congestion Dashboard",
     layout="wide"
 )
 
-# Define the pages
-pg1 = st.Page("pages/Disagreement_Traffic.py", title="Disagreement Analysis", icon="📊")
-pg2 = st.Page("pages/Izzati.py", title="Rural Perspectives", icon="🏘️")
-pg3 = st.Page("pages/Fathin.py", title="Urban Perspectives", icon="🏙️")
-pg4 = st.Page("pages/Khalida.py", title="Suburban Perspectives", icon="🏡")
+# Changed folder path from "pages/..." to "page/..."
+pg1 = st.Page("page/Disagreement_Traffic.py", title="Disagreement Analysis", icon="📊")
+pg2 = st.Page("page/Izzati.py", title="Rural Perspectives", icon="🏘️")
+pg3 = st.Page("page/Fathin.py", title="Urban Perspectives", icon="🏙️")
+pg4 = st.Page("page/Khalida.py", title="Suburban Perspectives", icon="🏡")
 
-# Initialize Navigation
+# Navigation
 pg = st.navigation([pg1, pg2, pg3, pg4])
 pg.run()
